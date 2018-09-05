0c80a1789fb00dff14125aba8825e994599b2687
Atul Singh <atulsolanki30@gmail.com>
2018-09-05 06:34:30 +0530

    First commit on go repo

---
+++ array.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [20]int
+	a[0] = 1
+	fmt.Println(a)
+}
---
+++ sum.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println(sum(12, 14))
+}
+
+func sum(a int, b int) int {
+
+	return a + b
+}
