736887e0ca62923f2fe7ac8ffc63fca29d841894
Atul Kumar Singh <atulsolanki30@gmail.com>
2024-02-08 13:35:21 +0530

    Added some Programs on array

--- array/checkCommonelements/main.go
+++ array/checkCommonelements/main.go
@@ -1,44 +1,60 @@
 // Intersection of the two arrays
+// Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays
+// and you may return the result in any order.
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func intersect(nums1 []int, nums2 []int) []int {
-	// Create frequency maps for each array
-	freqMap1 := make(map[int]int)
-	freqMap2 := make(map[int]int)
 
-	for _, num := range nums1 {
-		freqMap1[num]++
+	tempArray1 := make(map[int]int)
+	tempArray2 := make(map[int]int)
+
+	uniqueArray := make(map[int]bool)
+
+	var intersectArray []int
+
+	//iterate nums1 array and store data in tempArray1
+	for _, v := range nums1 {
+
+		tempArray1[v]++
+	}
+	for _, v := range nums2 {
+
+		tempArray2[v]++
 	}
 
-	for _, num := range nums2 {
-		freqMap2[num]++
-	}
+	for _, v := range nums1 {
 
-	// Find the intersection
-	var result []int
-	for num, count1 := range freqMap1 {
-		if count2, ok := freqMap2[num]; ok {
-			// Common element found, add to result the minimum frequency
-			minFreq := min(count1, count2)
-			for i := 0; i < minFreq; i++ {
-				result = append(result, num)
+		// check if value of nums1 array is set in tempArray2
+		if _, ok := tempArray2[v]; ok && !uniqueArray[v] {
+
+			m1 := tempArray1[v]
+			m2 := tempArray2[v]
+			num := min(m1, m2)
+
+			for i := 0; i < num; i++ {
+
+				intersectArray = append(intersectArray, v)
 			}
+			uniqueArray[v] = true
+
 		}
+
 	}
+	return intersectArray
 
-	return result
 }
-
 func main() {
 
-	nums1 := []int{1, 2, 3, 0, 9, 2}
+	nums1 := []int{1, 2, 3, 4, 5, 1, 2}
+	nums2 := []int{1, 2, 8, 9, 1, 1, 2}
 
-	nums2 := []int{2, 3, 4, 5, 8, 9, 2, 9, 8}
+	intersectArray := intersect(nums1, nums2)
 
-	array := intersect(nums1, nums2)
-
-	fmt.Println("The intersection of the array is ", array)
+	fmt.Println("The intersection of arrays is ", intersectArray)
 
 }
