fa2bb4447e2d05d7546241ecb969e308e52e229b
Aanvi Singh <atulsolanki30@gmail.com>
2024-03-21 15:08:38 +0530

    Added program for Singly Linked list

--- go.mod
+++ go.mod
@@ -6,4 +6,7 @@ require (
 	cloud.google.com/go/pubsub v1.12.2
 	github.com/gorilla/mux v1.8.0
 	github.com/rabbitmq/amqp091-go v0.0.0-20210623134908-fa1229110efb
+	github.com/rogpeppe/godef v1.1.2 // indirect
+	github.com/uudashr/gopkgs/v2 v2.1.2 // indirect
+	golang.org/x/tools v0.19.0 // indirect
 )
--- linkedList/main.go
+++ linkedList/main.go
@@ -1,71 +1,100 @@
+// Implement Linkedlist
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-// Node represent a node in Linked list
+//Structure for the node 
 type Node struct {
-	Data int
-	Next *Node
+	data int
+	next *Node
 }
 
-// LinkedList represent a singly linked list
+// Structure for LinkedList
 type LinkedList struct {
-	Head *Node
+	head *Node
 }
 
-// Insert insert a new node with the given data into the end of the Linked List
-func (ll *LinkedList) Insert(data int) {
-
-	newNode := &Node{Data: data}
-
-	if ll.Head == nil {
-		ll.Head = newNode
-		return
-	}
-	lastNode := ll.Head
-
-	for lastNode.Next != nil {
-		lastNode = lastNode.Next
-	}
-
-	lastNode.Next = newNode
-}
-
-func (ll *LinkedList) Display() {
-
-	current := ll.Head
-
-	for current != nil {
-
-		fmt.Println(current.Data)
-		current = current.Next
-	}
-
-}
 func main() {
 
-	//Create a linked list
-	linkedList := LinkedList{}
+	ll := LinkedList{}
+	//insert some elements at the end of the list
+	ll.insertAtTheEnd(20)
+	ll.insertAtTheEnd(30)
+	ll.insertAtTheEnd(40)
+	ll.insertAtTheEnd(50)
 
-	//var node *Node
+	//insert an element at the begining
+	ll.insertAtBegining(10)
 
-	//fmt.Println("The value of node variable ", node)
+	//insert an element after an element
+	ll.insertAfterValue(30,60)
 
-	// Insert a Element( a node) in Link List
-
-	linkedList.Insert(20)
-	//linkedList.Insert(30)
-	//linkedList.Insert(40)
-	//linkedList.Insert(50)
-	//linkedList.Insert(60)
-
-	// Display the elements from the Link List
-
-	//linkedList.Display()
+	ll.display()
 
 }
 
-//https://go.dev/play/p/seF4KK44S-4
-//https://go.dev/play/p/rsQmftCYP_I
+// insertAtBegining will insert a node at the begining of the lisnked list
+func  (ll *LinkedList) insertAtBegining(num int){
+	     node := &Node{data:num,next:nil}
+		// check if the list is empty, i.e ll.head is pointing to nil
+		 if ll.head ==nil{
+			 ll.head =node
+			 return 
+		 }
+		 
+		 node.next = ll.head //first point node next to head, this i to use head value
+		 ll.head = node     // head will point to given node  
+
+}
+
+//insertAfterValue will insert a node after a given node value
+
+func (ll *LinkedList) insertAfterValue(afterValue int, num int){
+
+	node := &Node{data:num, next:nil}
+	
+	ptr := ll.head
+	
+	for ptr != nil{
+		if(ptr.data == afterValue){	
+			node.next =ptr.next
+			ptr.next = node
+			return 
+		}
+		ptr = ptr.next
+	}
+	
+   fmt.Println("After values does not exit in the Linkedlist!")	   	
+}
+
+// insertAtTheEnd will insert the node at the end of the List
+
+func (ll *LinkedList) insertAtTheEnd(num int) {
+
+	tempNode := &Node{data: num}
+	if ll.head == nil {
+		ll.head = tempNode
+		fmt.Println("Node inserted successfully ", tempNode)
+		return
+
+	}
+	ptr := ll.head
+	for ptr.next != nil {
+		ptr = ptr.next
+
+	}
+	ptr.next = tempNode
+
+}
+
+// display will display all nodes of the linkedlist
+func (ll *LinkedList) display() {
+	ptr := ll.head
+
+	for ptr != nil {
+
+		fmt.Println(ptr.data)
+		ptr = ptr.next
+
+	}
+}
